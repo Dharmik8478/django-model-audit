41643e6a496aa4d8bba08eca108dd4dfcf6f4a17
Dharmik <dharmik8478@gmail.com>
2018-05-03 11:41:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-model-audit',
-    version='0.1',
+    version='0.1.7',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
